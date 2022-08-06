b01a1e93bd8bd3ba458eb548634f5ccdf4ac3be0
Ados <aden.huen@gmail.com>
2022-08-06 22:31:50 +0800

    fix tempdir using env variables

--- src/io/file.rs
+++ src/io/file.rs
@@ -1,13 +1,14 @@
 use core::result::Result;
+use std::env;
 use std::fs::File;
 use std::io::{Error, ErrorKind};
 use std::io::{Read, Write};
-use tempfile::{tempdir, TempDir};
+use std::path::PathBuf;
 
 const CFG_NAME: &str = "fl.cfg";
 
-fn get_file_data(d: TempDir) -> Result<String, Error> {
-    let file_path = d.path().join(CFG_NAME);
+fn get_cfg_data(d: PathBuf) -> Result<String, Error> {
+    let file_path = d.join(CFG_NAME);
     println!("{}", &file_path.to_string_lossy());
     let file_res = File::open(file_path);
     match file_res {
@@ -27,19 +28,8 @@ fn get_file_data(d: TempDir) -> Result<String, Error> {
 }
 
 pub fn set_config(url: String) -> Result<(), Error> {
-    let dir_result = tempdir();
-    let dir = match dir_result {
-        Ok(d) => d,
-        Err(e) => {
-            return match e.kind() {
-                ErrorKind::NotFound => Err(Error::new(ErrorKind::NotFound, e)),
-                ErrorKind::PermissionDenied => Err(Error::new(ErrorKind::PermissionDenied, e)),
-                _default => Err(Error::new(ErrorKind::Other, e)),
-            }
-        }
-    };
-
-    let file_path = dir.path().join(CFG_NAME);
+    let dir = env::temp_dir();
+    let file_path = dir.join(CFG_NAME);
     println!("{}", &file_path.to_string_lossy());
     let file_res = File::create(file_path);
     let mut file = match file_res {
@@ -57,13 +47,6 @@ pub fn set_config(url: String) -> Result<(), Error> {
 }
 
 pub fn get_config() -> Result<String, Error> {
-    let dir_result = tempdir();
-    match dir_result {
-        Ok(d) => get_file_data(d),
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => Err(Error::new(ErrorKind::NotFound, e)),
-            ErrorKind::PermissionDenied => Err(Error::new(ErrorKind::PermissionDenied, e)),
-            _default => Err(Error::new(ErrorKind::Other, e)),
-        },
-    }
+    let dir = env::temp_dir();
+    get_cfg_data(dir)
 }
