6a9e029cefe7115ab730b66a2464479a6f587d71
Ados <aden.huen@gmail.com>
2022-08-10 22:13:46 +0800

    Cleaner imports, reduce repeated code

--- src/io/web.rs
+++ src/io/web.rs
@@ -5,3 +5,14 @@ pub async fn get_web_body(url: String) -> Result<String, reqwest::Error> {
         Err(e) => Err(e),
     }
 }
+
+pub fn open_links(links: Vec<&str>) {
+    for link in links {
+        match open::that(link) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Something went wrong: {}", e)
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+mod data;
 mod io;
 mod parse;
-mod types;
+use crate::data::cmd::{Cli, Commands};
 use crate::io::file::{get_config, set_config};
-use crate::io::web::get_web_body;
-use crate::types::cmd::{Cli, Commands};
+use crate::io::web::{get_web_body, open_links};
 use clap::Parser;
-use parse::links::parse_from_cmd;
+use parse::links::parse_text;
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +15,7 @@ async fn main() {
         Commands::Config(cfg) => match &cfg.url {
             Some(link) => match set_config(link.to_string()) {
                 Ok(_) => {
-                    println!("Successfully updated config");
+                    println!("Successfully updated config to: {}", link);
                 }
                 Err(e) => {
                     println!("Error occured updating config: {}", e);
@@ -36,16 +36,8 @@ async fn main() {
         Commands::Open(text) => match &text.text {
             // If arguments are passed in, use string instead of url endpoint
             Some(text) => {
-                let links = parse_from_cmd(text);
-
-                for link in links {
-                    match open::that(link) {
-                        Ok(_) => {}
-                        Err(e) => {
-                            println!("Something went wrong: {}", e)
-                        }
-                    }
-                }
+                let links = parse_text(text);
+                open_links(links)
             }
             // If no arguments are passed in, load data from url endpoint
             None => match get_config() {
@@ -53,15 +45,8 @@ async fn main() {
                     let web_response = get_web_body(url).await;
                     match web_response {
                         Ok(body) => {
-                            let links = body.lines();
-                            for link in links {
-                                match open::that(link) {
-                                    Ok(_) => {}
-                                    Err(e) => {
-                                        println!("Something went wrong: {}", e)
-                                    }
-                                }
-                            }
+                            let links = parse_text(&body);
+                            open_links(links)
                         }
                         Err(e) => {
                             println!("Something went wrong: {}", e)
